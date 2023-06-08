dbf4a1812a451d0d28420eedfe8ddbdfb7f5abcb
K-BRIEL <cristiangabrielmesiano@gmail.com>
2023-06-08 15:09:16 -0300

    version 4

--- src/main/java/com/delOrigen/configurations/WebAuthorization.java
+++ src/main/java/com/delOrigen/configurations/WebAuthorization.java
@@ -2,6 +2,7 @@ package com.delOrigen.configurations;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,6 +23,7 @@ import java.util.ArrayList;
 
 @EnableWebSecurity
 @Configuration
+@EnableMethodSecurity
 class WebAuthorization extends WebSecurityConfigurerAdapter {
 
 
